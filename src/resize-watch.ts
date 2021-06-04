9c29cee3a28c9eafbc3d232c9f8265ee27799250
pengkaiyang <476922814@qq.com>
2021-06-05 01:02:05 +0800

    update

--- src/resize-watch.ts
+++ src/resize-watch.ts
@@ -1,44 +1,43 @@
-import {remove} from './remove';
+import { remove } from './remove'
 
-export type Callback = (rect: DOMRect, oldRect: DOMRect) => unknown;
+export type Callback = (rect: DOMRect, oldRect: DOMRect) => unknown
 
 interface Item {
-  callbacks: Callback[];
-  contentRect: DOMRect
+    callbacks: Callback[]
+    contentRect: DOMRect
 }
 
-const elAndCbMap: Map<Element, Item> = new Map();
+const elAndCbMap: Map<Element, Item> = new Map()
 
 const ro = new ResizeObserver(entries => {
-  const elAndCbEntries = Array.from(elAndCbMap.entries());
-  entries.forEach((entry) => {
-    const i = elAndCbEntries.findIndex(([el]) => el === entry.target);
-    if (i !== -1) {
-      const [, item] = elAndCbEntries[i];
-      const { contentRect: oldRect, callbacks: fns } = item;
-      item.contentRect = entry.contentRect;
-      fns.forEach(fn => {
-        fn(item.contentRect, oldRect);
-      });
-    }
-  });
-});
+    const elAndCbEntries = Array.from(elAndCbMap.entries())
+    entries.forEach(entry => {
+        const i = elAndCbEntries.findIndex(([el]) => el === entry.target)
+        if (i !== -1) {
+            const [, item] = elAndCbEntries[i]
+            const { contentRect: oldRect, callbacks: fns } = item
+            item.contentRect = entry.contentRect
+            fns.forEach(fn => {
+                fn(item.contentRect, oldRect)
+            })
+        }
+    })
+})
 
-export function resizeWatch(callback: Callback, el: Element = document.documentElement) {
-  const item: Item = elAndCbMap.get(el) ?? {
-    contentRect: el.getBoundingClientRect(),
-    callbacks: [] as Callback[],
-  };
-  const fns = item.callbacks;
-  fns.push(callback);
-  elAndCbMap.set(el, item);
-  ro.observe(el);
-  return () => {
-    remove(fns, (cb: Callback) => cb === callback);
-    if (!fns.length) {
-      elAndCbMap.delete(el);
-      ro.unobserve(el);
+export function resizeWatch(el: Element, callback: Callback) {
+    const item: Item = elAndCbMap.get(el) ?? {
+        contentRect: el.getBoundingClientRect(),
+        callbacks: [] as Callback[]
     }
-  };
+    const fns = item.callbacks
+    fns.push(callback)
+    elAndCbMap.set(el, item)
+    ro.observe(el)
+    return () => {
+        remove(fns, (cb: Callback) => cb === callback)
+        if (!fns.length) {
+            elAndCbMap.delete(el)
+            ro.unobserve(el)
+        }
+    }
 }
-
