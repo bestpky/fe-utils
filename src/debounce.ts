1abb7e800e5531d763a0a0c54da0702940d99bc0
pengkaiyang <476922814@qq.com>
2021-06-05 19:25:25 +0800

    节流防抖

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,1 +1,9 @@
-export function debounce() {}
+export function debounce(fn: Function, delay: number) {
+    let timeout: NodeJS.Timeout
+    return (...args: any[]) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+            fn(...args)
+        }, delay)
+    }
+}
--- src/throttle.ts
+++ src/throttle.ts
@@ -1,1 +1,16 @@
-export function throttle() {}
+export function throttle(fn: Function, delay: number) {
+    let timer: NodeJS.Timeout, last: number
+    return (...args: any[]) => {
+        const now = Date.now()
+        if (last && last + delay > now) {
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                last = now
+                fn(...args)
+            }, delay)
+        } else {
+            last = now
+            fn(...args)
+        }
+    }
+}
